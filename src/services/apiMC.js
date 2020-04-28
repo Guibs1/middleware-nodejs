6125629a7a65b8080a0a3a3cca89ede3a4b78b32
Guilherme <guilherme@MacBook-Pro.local>
2020-04-27 23:05:08 -0300

    creating test insertDE

--- src/services/apiMC.js
+++ src/services/apiMC.js
@@ -1,7 +1,7 @@
 require('dotenv/config'); //temporary
 const axios = require('axios');
 
-const authentication = async () => {
+const Authenticate = async () => {
     try {
         return await axios.post(`${process.env.APIMC_BASEURLAUTH}`, {
             grant_type: `${process.env.APIMC_GRANT_TYPE}`,
@@ -9,13 +9,23 @@ const authentication = async () => {
             client_secret: `${process.env.APIMC_CLIENTSECRET}`
         });
     } catch (error) {
-        console.error(error)
+        console.error(error);
     }
 }
 
-async function getTokenMC() {
-    const token = await authentication();
-    return token.data.access_token;
+async function inserInDE(Users, Authenticate) {
+    try {
+        const token = await Authenticate();
+        const baseUrlAuthorized = await axios.post(`${token.data.rest_instance_url}/data/v1/async/dataextensions/key:${process.env.APIMC_DATAEXTENSION_KEY}/rows`, {
+            headers: {
+              'Authorization': `${token.data.token_type} ${token.data.access_token}`
+            },
+            body: Users
+        });
+        return baseUrlAuthorized;
+    } catch (error) {
+        console.error(error);
+    }
 }
-console.log(getTokenMC());
+
 //module.exports = getTokenMC;
